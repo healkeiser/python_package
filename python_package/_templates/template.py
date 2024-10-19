b7906bf2cadc95d401b4472cc511521dda24015d
Valentin Beaumont <valentin.onze@gmail.com>
2024-10-19 14:35:22 +0100

    Added template

---
+++ python_package/_templates/template.py
@@ -0,0 +1,34 @@
+"""Template module."""
+
+# Built-in
+import logging
+
+# Third-party
+import numpy as np
+
+# Internal
+from python_package import main
+
+# Constants
+PI = np.pi
+
+# Logger
+logger = logging.getLogger(__name__)
+
+
+def hello_world() -> None:
+    """Prints a greeting."""
+
+    print("Hello, world!")
+
+
+class HelloWorld:
+    """A simple class with a single attribute."""
+
+    def __init__(self):
+        self.to_print = "Hello, world!"
+
+    def print(self) -> None:
+        """Prints the attribute value."""
+
+        print(self.to_print)
---
+++ python_package/main.py
@@ -0,0 +1,14 @@
+"""Main entry point for the package."""
+
+
+def hello():
+    """Prints a greeting."""
+
+    print("Hello, world!")
+
+
+class ClassA:
+    """A simple class with a single attribute."""
+
+    def __init__(self):
+        self.value = 0
